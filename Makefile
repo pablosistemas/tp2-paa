4eb0da5010e1b021c1557b6133d0fbb04ec81929
pablosistemas <pablogoulart13@gmail.com>
2018-05-12 17:48:17 -0300

    Implementação 2716

--- Makefile
+++ Makefile
@@ -1,15 +1,19 @@
 CC=g++
 CFLAGS=-Wall -std=c++14
 
-all: 2095 2115 1927
+all: 2716 2115  2095 1927
 
-2095: src/main2095/main.cc
+2716:
 	mkdir bin || true
-	$(CC) $(CFLAGS) src/main2095/main.cc -o bin/2095
+	$(CC) $(CFLAGS) src/main2716/main.cc -o bin/2716
 
 2115:
 	mkdir bin || true
 	$(CC) $(CFLAGS) src/main2115/main.cc -o bin/2115
+
+2095: src/main2095/main.cc
+	mkdir bin || true
+	$(CC) $(CFLAGS) src/main2095/main.cc -o bin/2095
 
 1927:
 	mkdir bin || true
---
+++ src/main2716/main.cc
@@ -0,0 +1,40 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+
+int main() {
+    uint32_t N;
+    std::vector<uint32_t> pesos_trabalhos;
+
+    while(fscanf(stdin, "%u\n", &N) != EOF) {
+        uint32_t ptr_rangel, ptr_gugu;
+        pesos_trabalhos.resize(N);
+
+        for (uint32_t i = 0; i < N - 1; i++) {
+            fscanf(stdin, "%d ", &pesos_trabalhos[i]); 
+        }
+        fscanf(stdin, "%d\n", &pesos_trabalhos[N-1]); 
+
+        uint32_t soma_rangel = pesos_trabalhos[0];
+        uint32_t soma_gugu = pesos_trabalhos[N-1];
+        uint32_t num_trabalhos_repartidos = 2;
+        ptr_rangel = 1;
+        ptr_gugu = N-2;
+
+        while(num_trabalhos_repartidos < N) {
+            while(soma_rangel < soma_gugu && ptr_rangel <= ptr_gugu) {
+                soma_rangel += pesos_trabalhos[ptr_rangel++];
+                num_trabalhos_repartidos++;
+            }
+            while(soma_gugu < soma_rangel && ptr_rangel <= ptr_gugu) {
+                soma_gugu += pesos_trabalhos[ptr_gugu++];
+                num_trabalhos_repartidos++;
+            }
+        }
+
+        fprintf(stdout, "%u\n", (uint32_t)std::abs(soma_gugu - soma_rangel));
+    }
+
+    return 0;
+}
