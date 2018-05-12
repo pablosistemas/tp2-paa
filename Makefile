847519f52bbeffca4eff81b282d202f1efc5bd22
pablosistemas <pablogoulart13@gmail.com>
2018-05-12 15:46:06 -0300

    Implementação 1927, 2095, 2115

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=g++
 CFLAGS=-Wall -std=c++14
 
-all: 2095 2115
+all: 2095 2115 1927
 
 2095: src/main2095/main.cc
 	mkdir bin || true
@@ -9,7 +9,11 @@ all: 2095 2115
 
 2115:
 	mkdir bin || true
-	$(CC) $(CFLAGS) src/main2095/main.cc -o bin/2115
+	$(CC) $(CFLAGS) src/main2115/main.cc -o bin/2115
+
+1927:
+	mkdir bin || true
+	$(CC) $(CFLAGS) src/main1927/main.cc -o bin/1927
 
 clean:
 	rm -rf bin
--- src/main1927/main.cc
+++ src/main1927/main.cc
@@ -1,17 +1,112 @@
-#include <stdio.h>
 #include <inttypes.h>
+#include <algorithm>
+#include <stdio.h>
+#include <vector>
+#include <limits>
+#include <tuple>
+
+#define TEMPO_MAXIMO 1000
+#define COORD 10
+
+std::vector<std::vector<std::vector<uint32_t> > > dp(COORD+1);
+std::vector<std::tuple<uint32_t, uint32_t, uint32_t> > tapioca;
+
+uint32_t NAO_INICIALIZADO = std::numeric_limits<uint32_t>::max();
+
+bool eh_solucao(std::tuple<uint32_t, uint32_t, uint32_t>& coordenada) {
+    auto it = std::find_if(tapioca.begin(), tapioca.end(), 
+        [&](std::tuple<uint32_t, uint32_t, uint32_t>& t) {
+        return std::get<0>(t) == std::get<0>(coordenada) && 
+               std::get<1>(t) == std::get<1>(coordenada) && 
+               std::get<2>(t) == std::get<2>(coordenada);
+    });
+    if (it != tapioca.end()) {
+        return true;
+    }
+    return false;
+}
+
+uint32_t dp_a(int32_t x, int32_t y, uint32_t tempo, uint32_t positivo) {
+    if (x < 0 || x > COORD || y < 0 || y > COORD || tempo > TEMPO_MAXIMO) {
+        return 0;
+    }
+
+    if (dp[x][y][tempo] != NAO_INICIALIZADO) {
+        return dp[x][y][tempo];
+    }
+
+    int32_t num_solucoes = std::max(
+        dp_a(x-1, y, tempo+1,positivo),
+        std::max(dp_a(x, y-1, tempo+1,positivo),
+        std::max(dp_a(x+1, y, tempo+1,positivo),
+            dp_a(x, y+1, tempo+1,positivo))));
+
+    auto tupla = std::make_tuple<uint32_t, uint32_t, uint32_t>((uint32_t)x, (uint32_t)y, (uint32_t)tempo);
+    if (eh_solucao(tupla)) {
+        if (dp[x][y][tempo] != NAO_INICIALIZADO) {
+            if ((uint32_t)(num_solucoes + 1) > dp[x][y][tempo]){
+                dp[x][y][tempo] = (uint32_t)num_solucoes + 1;
+            }
+        } else {
+            dp[x][y][tempo]= (uint32_t)num_solucoes + 1;
+        }
+    } else {
+        if (dp[x][y][tempo] == NAO_INICIALIZADO || (uint32_t)num_solucoes > dp[x][y][tempo]) {
+            dp[x][y][tempo] = (uint32_t)num_solucoes;
+        }
+    }
+    return dp[x][y][tempo];
+}
 
 int main() {
     FILE* entrada;
-    entrada = fopen("data/entrada", "r");
+    entrada = fopen("data/entrada1927", "r");
 
     uint32_t n;
-    int estado_entrada = fscanf(entrada, "%u\n", n);
+    fscanf(entrada, "%u\n", &n);
 
-    for(int i = 0; i < n; i++) {
-        
+    for(auto it = dp.begin(); it != dp.end(); it++){
+        it->resize(COORD + 1);
+        for(auto iti = it->begin(); iti != it->end(); iti++){
+            iti->resize(1001);
+            for(auto itii = iti->begin(); itii != iti->end(); itii++) {
+                *itii = NAO_INICIALIZADO;
+            }
+        }
     }
+
+    tapioca.resize(n);
+    for(uint32_t i = 0; i < n; i++) {
+        uint32_t x, y, t;
+        fscanf(entrada, "%u %u %u\n", &x, &y, &t);
+        tapioca[i] = std::make_tuple(x, y, t);
+    }
+
+    dp_a(6, 6, 0, 0);
+
+    fprintf(stdout, "%u\n", dp[6][6][0]);
 
     fclose(entrada);
 
+    return 0;
 }
+
+/*
+
+    auto s1 = eh_solucao(tapioca[0]);
+    auto s2 = eh_solucao(tapioca[1]);
+    auto s3 = eh_solucao(tapioca[2]);
+    auto tupla_teste = std::make_tuple<uint32_t, uint32_t, uint32_t>(1,1,1);
+    auto s4 = eh_solucao(tupla_teste);
+
+    auto max = 0; 
+    for (auto it = tapioca.begin(); it != tapioca.end(); it++) {
+        if (dp[std::get<0>(*it)][std::get<1>(*it)][std::get<2>(*it)] > max) {
+            max = dp[std::get<0>(*it)][std::get<1>(*it)][std::get<2>(*it)];
+        }
+    }
+
+        if (dp[x][y][tempo] < positivo){
+            dp[x][y][tempo] = positivo;
+        }
+*/
